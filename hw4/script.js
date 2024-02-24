d0bc265e64cea1a657c8ec34b1f50a91e30f472b
ChrisJacobs2 <cmjacobs@uncg.edu>
2024-02-23 21:29:07 -0500

    almost done

--- hw4/script.js
+++ hw4/script.js
@@ -6,10 +6,19 @@ const scissors = document.getElementById('scissors');
 const choices = [rock, paper, scissors];
 
 
+// variables for the computer's thinking animation
+const question = document.getElementById('question');
+const rockAnim = document.getElementById('rock-anim');
+const paperAnim = document.getElementById('paper-anim');
+const scissorsAnim = document.getElementById('scissors-anim');
+const anims = [rockAnim, paperAnim, scissorsAnim];
+
+
+
 // flag to keep track of whether the computer is thinking
 var gameOngoing = false;
 
-var result;
+var result; // the result of the game. This is either "Victory", "Defeat", or "Tie".
 var wins = 0;
 var losses = 0;
 var ties = 0;
@@ -21,7 +30,7 @@ choices.forEach(choice => {
             gameOngoing = true;        
             // play the game
             main(choice.id);
-
+            
             // highlight the choice
             choice.classList.add('highlight');
             // unhighlight it after 3 seconds
@@ -33,6 +42,19 @@ choices.forEach(choice => {
     });
 });
 
+/**
+ * This function runs the game given an element id in the form of a string
+ * @param {string} playerChoice - the player's choice of rock, paper, or scissors
+ */
+function main(playerChoice) {
+    const cpuChoice = computerChoice();
+    result = playGame(playerChoice, cpuChoice);
+    // animate the computer's choice
+    computerThinking(cpuChoice);
+        // TODO: Make the box I clicked highlighted until the computer makes a choice
+    updateResult(result);
+    
+}
 
 /**
  * This function takes two string parameters, the player's choice and the computer's
@@ -41,6 +63,7 @@ choices.forEach(choice => {
  * @returns the result of the game as a string, 'Victory!', 'Defeat!', or 'Tie!'
  */
 function playGame(playerChoice, computerChoice) {
+    console.log(playerChoice, computerChoice);
     // 3 possibilities for a tie
     if (playerChoice == computerChoice) {
         return 'Tie!';
@@ -57,21 +80,22 @@ function playGame(playerChoice, computerChoice) {
 }
 
 /**
- * This function generates a number from 1 to 3, and returns the computer's choice
- * reflecting that number. 0/1 is rock, 2 is paper, and 3 is scissors.
+ * This function generates a number from 0 to 2, and returns the computer's choice as 
+ * a string reflecting that number. 0 is rock, 1 is paper, and 2 is scissors.
  * @returns the computer's choice of rock, paper, or scissors as a string
  */
 function computerChoice() {
-    // rand is a real number from 0 to 3, not including 3
+    // rand is a real number from 0 to 2.
     var rand = Math.random() * 3;
-    // rand is now an interger from 1 to 3. Pretty low (but nonzero) chance of it being 0.
-    rand = Math.ceil(rand);
-    if (rand == 1 || rand == 0) {
-        return 'rock';
-    } else if (rand == 2) {
-        return 'paper';
-    } else {    //rand == 3
-        return 'scissors';
+
+    rand = Math.floor(rand);
+    switch (rand) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
     }
 }
 
@@ -97,19 +121,6 @@ function updateResult(result) {
     // TODO: Add a reset button
 }
 
-/**
- * This function runs the game given an element id in the form of a string
- * @param {string} playerChoice - the player's choice of rock, paper, or scissors
- */
-function main(playerChoice) {
-    const cpuChoice = computerChoice();
-    result = playGame(playerChoice, cpuChoice);
-    // TODO: run an animation to show the computer thinking
-    computerThinking();
-        // TODO: Make the box I clicked highlighted until the computer makes a choice
-    updateResult(result);
-    // TODO: Run an animation to show the result of the game
-}
 
 
 
@@ -122,18 +133,71 @@ function main(playerChoice) {
  * for three seconds. Then, the image will be replaced with the computer's choice.
  * While the computer is 'thinking', the player's choice will be highlighted. They will
  * also not be able to pick another choice until the computer has made its choice.
+ * @param {string} cpuChoice - the computer's choice of "rock", "paper", or "scissors"
  */
-function computerThinking() {
+function computerThinking(cpuChoice) {
     // Set the pointer to default for the player's choices
     choices.forEach(choice => {
         choice.style.cursor = 'default';
     });
-    // TODO: Make it so I can't click the images while the computer is thinking    
-            // TODO: Use loop to change the image 3 times a half-second cycle, looping 6 times
-            // TODO: display the computer's choice
+
+    shuffleAnim(cpuChoice);
             // TODO: Make it so the play-again button needs to be hit to reset the game
     choices.forEach(choice => {
         choice.style.cursor = 'pointer';
     });
     thinking = false;
+}
+
+/**
+ * This function is called by computerThinking() to animate
+ * the shuffling of the computer throw. It works by unhiding and hiding
+ * different images of rock, paper, and scissors in order.
+ */
+function shuffleAnim(cpuChoice) {
+    // I know this line looks cringe but its more readable and was easier to code
+    var arr = [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2];
+
+    // hide the question mark in 1/6 of a second from now
+    setTimeout(() => {
+        question.style.display = 'none';
+    }, 167);
+
+    // Interval with 1/6 second delay.
+    var x = 0;
+    var clock = setInterval(function() {
+        
+        // Hide the previously shown image
+        if (x > 0) {
+            anims[arr[x - 1]].style.display = 'none';
+        }
+
+        // Show the next image
+        anims[arr[x]].style.display = 'block';
+        
+        x++;
+        if (x >= 18) {
+            clearInterval(clock); // stop the interval
+            displayComputerChoice(cpuChoice);
+        }
+    }, 167); // 167 ms is 1/6 of a second
+    
+}
+
+function displayComputerChoice(cpuChoice) {
+    // hide all anims
+    rockAnim.style.display = 'none';
+    paperAnim.style.display = 'none';
+    scissorsAnim.style.display = 'none';
+    switch (cpuChoice) {
+        case 'rock':
+            rockAnim.style.display = 'block';
+            break;
+        case 'paper':
+            paperAnim.style.display = 'block';
+            break;
+        case 'scissors':
+            scissorsAnim.style.display = 'block';
+            break;
+    }
 }
